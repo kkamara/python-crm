f11ee9fbca0d86ab3467f61dec7ddc68bd2d43c0
kkamara <kamaracomputers@gmail.com>
2023-08-12 23:55:02 +0100

    Add User model

--- crm_users/models.py
+++ crm_users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from datetime import datetime
+
+class User(models.Model):
+    user_created = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255, unique=True)
+    contact_number = models.CharField(max_length=255, blank=True)
+    street_name = models.CharField(max_length=255, blank=True)
+    building_number = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    postcode = models.CharField(max_length=255, blank=True)
+    date_updated = models.DateTimeField(default=datetime.now, blank=True)
