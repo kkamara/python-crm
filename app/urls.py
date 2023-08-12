22f6f2bda1f83b89a02c66e26131040f5e757b1b
kkamara <kamaracomputers@gmail.com>
2023-08-12 22:33:26 +0100

    Cleanup

--- app/urls.py
+++ app/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
